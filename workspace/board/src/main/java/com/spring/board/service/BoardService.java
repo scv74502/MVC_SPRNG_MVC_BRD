ee7937808678388bbef7dec19bf20f4937656cff
park <scv74502@anver.com>
2020-02-12 11:17:39 +0900

    15까지 완료

--- workspace/board/src/main/java/com/spring/board/service/BoardService.java
+++ workspace/board/src/main/java/com/spring/board/service/BoardService.java
@@ -11,82 +11,84 @@ import com.spring.board.form.BoardForm;
 
 @Service
 public class BoardService {
-	
-	@Autowired 
+
+	@Autowired
 	private BoardDao boardDao;
-	
+
+	/** 게시판 - 목록 조회 */
 	public List<BoardDto> getBoardList(BoardForm boardForm) throws Exception {
+
 		return boardDao.getBoardList(boardForm);
 	}
-	
+
 	/** 게시판 - 상세 조회 */
 	public BoardDto getBoardDetail(BoardForm boardForm) throws Exception {
-		
+
 		BoardDto boardDto = new BoardDto();
-		
-		String serarchType = boardForm.getSearch_type();
-		
-		if("S".equals(serarchType)) {
+
+		String searchType = boardForm.getSearch_type();
+
+		if("S".equals(searchType)){
 			
 			int updateCnt = boardDao.updateBoardHits(boardForm);
-			
+		
 			if (updateCnt > 0) {
 				boardDto = boardDao.getBoardDetail(boardForm);
-				
 			}
+			
 		} else {
 			
 			boardDto = boardDao.getBoardDetail(boardForm);
 		}
-		
+
 		return boardDto;
 	}
-	
+
 	/** 게시판 - 등록 */
 	public BoardDto insertBoard(BoardForm boardForm) throws Exception {
 
 		BoardDto boardDto = new BoardDto();
-		
+
 		int insertCnt = boardDao.insertBoard(boardForm);
-		
+
 		if (insertCnt > 0) {
 			boardDto.setResult("SUCCESS");
 		} else {
 			boardDto.setResult("FAIL");
-		} 
-		
+		}
+
 		return boardDto;
-	} 
-	
+	}
+
 	/** 게시판 - 삭제 */
 	public BoardDto deleteBoard(BoardForm boardForm) throws Exception {
 
 		BoardDto boardDto = new BoardDto();
-		
+
 		int deleteCnt = boardDao.deleteBoard(boardForm);
-		
+
 		if (deleteCnt > 0) {
 			boardDto.setResult("SUCCESS");
 		} else {
 			boardDto.setResult("FAIL");
-		} 
-		
+		}
+
 		return boardDto;
-	} 
-	
+	}
+
 	/** 게시판 - 수정 */
 	public BoardDto updateBoard(BoardForm boardForm) throws Exception {
 
 		BoardDto boardDto = new BoardDto();
-		
-		int updateCnt = boardDao.updateBoard(boardForm);
-		
-		if (updateCnt > 0) {
+
+		int deleteCnt = boardDao.updateBoard(boardForm);
+
+		if (deleteCnt > 0) {
 			boardDto.setResult("SUCCESS");
 		} else {
 			boardDto.setResult("FAIL");
-		} 
-		
+		}
+
 		return boardDto;
 	}
 }
